d80e61a17122b7225a9008ed3572e4f61e32cbdf
Mahigayu <123053121+Mahigayu@users.noreply.github.com>
2023-01-19 14:41:29 +0530

    first commit add

---
+++ Add.java
@@ -0,0 +1,9 @@
+class Add{
+public static void Main(String args[]){
+int a=10;
+int b=5;
+System.out.println("add of 2 number "+(a+b));
+System.out.println("demo for addition ");
+System.out.println("demo for addition ");
+}
+}
