3b33138db8e663873c6a3a7f7d7defac09a4cbf7
Mahigayu <gayathriselva60@gmail.com>
2023-01-19 14:50:53 +0530

    changes in sub branch

--- Add.java
+++ Add.java
@@ -2,6 +2,7 @@ class Add{
 public static void Main(String args[]){
 int a=10;
 int b=5;
+System.out.println("****Add**");
 System.out.println("add of 2 number "+(a+b));
 System.out.println("demo for addition ");
 System.out.println("demo for addition ");
